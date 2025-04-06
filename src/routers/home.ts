2b4ee88bdb3ec91b3c9a5fa81f7382f299b8e555
AllenLiDeveloper <allen.li.service@gmail.com>
2025-04-06 15:01:28 +0800

    feat: initial setup with Vue and Vite

---
+++ src/routers/home.ts
@@ -0,0 +1,20 @@
+import type { RouteRecordRaw } from "vue-router";
+
+const homeRouter: RouteRecordRaw[] = [
+  {
+    path: "/",
+    component: () => import("../layouts/AppLayout.vue"),
+    meta: {
+      authenticated: true,
+    },
+    children: [
+      {
+        path: "",
+        name: "Home.Main",
+        component: () => import("../views/Home.vue"),
+      },
+    ],
+  },
+];
+
+export { homeRouter };
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <h1>Welcome to Bluesky Frontend</h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.home {
+  padding: 20px;
+}
+</style>
