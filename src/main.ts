5cde7dba3eb9fdad47a404c98bbfb8643df3d3d8
AllenLi <96935173+AllenLi0110@users.noreply.github.com>
2025-04-09 23:24:25 +0800

    feat: add prettier (#2)

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import "./style.css";
-import App from "./App.vue";
-import naive from "naive-ui";
-import router from "./routers";
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import './style.css'
+import App from './App.vue'
+import naive from 'naive-ui'
+import router from './routers'
 
-const app = createApp(App);
-app.use(createPinia());
-app.use(naive);
-app.use(router);
-app.mount("#app");
+const app = createApp(App)
+app.use(createPinia())
+app.use(naive)
+app.use(router)
+app.mount('#app')
